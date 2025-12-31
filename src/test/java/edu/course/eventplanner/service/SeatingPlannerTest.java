82152c1b13f43334e695aff16e155c198ac49fed
blerner5 <blerner5@student.touro.edu>
2025-12-30 20:04:43 -0500

    bad code
    bad code...lots of errors

--- src/test/java/edu/course/eventplanner/service/SeatingPlannerTest.java
+++ src/test/java/edu/course/eventplanner/service/SeatingPlannerTest.java
@@ -1,9 +1,11 @@
 package edu.course.eventplanner.service;
 
+import edu.course.eventplanner.model.Guest;
 import edu.course.eventplanner.model.Venue;
 import org.junit.jupiter.api.Test;
-import edu.course.eventplanner.model.Guest;
-import java.util.*;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class SeatingPlannerTest {
@@ -12,8 +14,26 @@ public class SeatingPlannerTest {
     public void testSeatingPlanner() {
         Venue venue = new Venue("Palace", "10,000", "500", "50", "10");
 
-        AssertEquals("Palace", "10,000", "500", "50", "10");
-
+        assertEquals("Palace", venue.getName());
+        assertEquals( "10,000", venue.getCost());
+        assertEquals("500", venue.getTables());
+        assertEquals("50", venue.getTables();
+        assertEquals("10",  venue.getSeatsPerTable());
     }
-
 }
+
+    @Test
+    public void generateSeatingTest() {
+        List<Guest> guests = new ArrayList<>();
+        for (int i = 1; i <= 25; i++) {
+            guests.add(new Guest("Guest" + i, "General"));
+        }
+
+        List<Table> tables = planner.generateSeating(guests, venue);
+
+        assertEquals(3, tables.size());
+        assertEquals(10, tables.get(0).getGuests().size());
+        assertEquals(10, tables.get(1).getGuests().size());
+        assertEquals(5, tables.get(2).getGuests().size());
+}
+
