2c440eaacbea557f2a03072f817c3003c66624bc
blerner5 <blerner5@student.touro.edu>
2026-01-14 23:23:15 -0500

    renamed my student test

--- src/test/java/MyStudentTest.java
+++ src/test/java/MyStudentTest.java
@@ -3,7 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class StudentCoverageTest {
+public class MyStudentTest {
     @Test
     void testVenueGetterCoverage() {
         Venue v = new Venue("TestVenue", 100, 10, 5, 2);
