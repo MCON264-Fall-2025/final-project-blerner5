edaa900654cb8be6ba42003df9d9820c1da3eeea
blerner5 <blerner5@student.touro.edu>
2026-01-14 23:15:41 -0500

    added a venue constructor class
    added constructor class

---
+++ src/test/java/VenueConstructorTest.java
@@ -0,0 +1,12 @@
+import edu.course.eventplanner.model.Venue;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class VenueConstructorTest {
+    @Test
+    void testVenueGetterCoverage() {
+        Venue v = new Venue("TestVenue", 100, 10, 5, 2);
+        assertEquals(10, v.getCapacity());
+    }
+}
